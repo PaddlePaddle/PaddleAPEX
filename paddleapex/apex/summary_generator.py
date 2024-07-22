8c9ee8a119eff849d89b6250165ab5fa32b0f4d8
Xujinming <xujinming@baidu.com>
2024-07-22 19:48:13 +0800

    update memory view

--- paddleapex/apex/summary_generator.py
+++ paddleapex/apex/summary_generator.py
@@ -14,11 +14,19 @@ parser.add_argument(
     type=str,
     required=True,
 )
+parser.add_argument(
+    "-mem",
+    dest="mem_detail",
+    type=str,
+    required=True,
+)
 args = parser.parse_args(sys.argv[1:])
 acc_result_path = args.acc_result
 prof_detail_path = args.prof_detail
+mem_detail_path = args.mem_detail
 acc_summary = csv.reader(open(acc_result_path, 'r'))
 prof_details = csv.reader(open(prof_detail_path, 'r'))
+mem_details = csv.reader(open(mem_detail_path, 'r'))
 
 acc_dict = {}
 acc_summary = list(acc_summary)
@@ -31,8 +39,19 @@ for i in range(len(acc_summary)):
         acc_dict[fwd_name] = acc_summary[i][1]
         acc_dict[bwd_name] = acc_summary[i][2]
 
+mem_dict = {}
+mem_details = list(mem_details)
+for i in range(len(mem_details)):
+    if i == 0:
+        continue
+    else:
+        fwd_name = mem_details[i][0] + ".forward"
+        mem_dict[fwd_name] = [mem_details[i][1], mem_details[i][2]]
+
+
+
 prof_details = list(prof_details)
-csv_head = tuple(prof_details[0]).__add__(tuple(["Acc_status"]))
+csv_head = tuple(prof_details[0])+(tuple(["Acc_status", "Bench_memory", "Device_memory"]))
 for item in prof_details[1:]:
     name = item[0]
     try:
@@ -40,6 +59,20 @@ for item in prof_details[1:]:
     except:
         status = "N/A"
     item.append(status)
+
+for item in prof_details[1:]:
+    name = item[0]
+    try:
+        bench_mem = mem_dict[name][0]
+    except:
+        bench_mem = ""
+    item.append(bench_mem)
+
+    try:
+        device_mem = mem_dict[name][1]
+    except:
+        device_mem = ""
+    item.append(device_mem)
 
 with open('prof_summary.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
